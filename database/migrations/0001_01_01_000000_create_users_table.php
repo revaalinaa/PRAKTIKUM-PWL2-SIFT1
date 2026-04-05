5b5b677847dca9b6facc50049cc70b7f626ee80f
Revalina Annisa Putri Asmara <revaalinaa2@gmail.com>
2026-04-05 13:55:15 +0700

    Add files via upload
    T1-PRAKTIKUM

---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->integer('npm')->primary();
+            $table->string('username');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
+};
